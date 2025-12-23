1ec1d4b42f4fde0dd00ae878cb6942717f8a0e8e
Vinay Arjun Hajare <vinayhajare2004@gmail.com>
2025-12-23 11:44:29 +0530

    feat: initialize chess-llama-ui project with Vite and TypeScript
    - Add package.json with dependencies for Vite, TypeScript, and chess libraries.
    - Include Vite logo SVG in the public directory.
    - Implement Llama model integration for chess move predictions in llama.ts.
    - Set up main game logic in main.ts, including chessboard rendering and move handling.
    - Create styles for the application in style.css, ensuring a dark theme and responsive design.
    - Add TypeScript environment definitions in vite-env.d.ts.
    - Configure TypeScript settings in tsconfig.json for strict type checking and module resolution.
    - Set up Vite configuration in vite.config.ts for building the project.

---
+++ src/llama.ts
@@ -0,0 +1,69 @@
+import { LlamaForCausalLM, LlamaTokenizer, topk } from '@huggingface/transformers';
+import { } from "@huggingface/transformers"
+import type { Key } from 'chessground/types';
+
+let tokenizer = await LlamaTokenizer.from_pretrained('lazy-guy12/chess-llama');
+let model = await LlamaForCausalLM.from_pretrained('lazy-guy12/chess-llama', {device: "wasm", dtype: "q4"});
+
+let k = tokenizer.model.vocab.length
+
+let mid = 0;
+
+const diffSelector:HTMLSelectElement = document.getElementById("difficulty")! as HTMLSelectElement;
+diffSelector.addEventListener("change", () => {
+	mid = parseInt(diffSelector.value);
+})
+
+async function wait(ms: number): Promise<void> {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+export default async function playLlama(moves: string[], dests: Map<Key, Key[]>): Promise<string> {
+	const start = performance.now();
+	let inputs = await tokenizer(moves.join(" "));
+	let { logits } = await model(inputs);
+	let preds = logits.slice(null, -1, null);
+
+
+	const [_v, {data}] = await topk(preds, k);
+	const end = performance.now();
+	console.log(`Inference took ${end - start} ms`);
+
+	if(end - start < 1000) {
+		await wait(1000 - (end - start));
+	}
+
+	let move: string = "0000";
+
+	for(let i = mid; i >= 0; i--) {
+		move = tokenizer.decode([data[i]]);
+		if (move.length >= 4) {
+			let s = move.substring(0, 2) as Key
+			let e = move.substring(2, 4) as Key
+			if(dests.has(s)) {
+				if(dests.get(s)?.includes(e)) {
+					break;
+				}
+			}
+		}
+	}
+
+	if(move != "0000")
+		return move;
+
+	for (let i = mid + 1; i < data.length; i++) {
+		move = tokenizer.decode([data[i]]);
+		if (move.length >= 4) {
+			let s = move.substring(0, 2) as Key
+			let e = move.substring(2, 4) as Key
+			if(dests.has(s)) {
+				if(dests.get(s)?.includes(e)) {
+					break;
+				}
+			}
+		}
+	}
+
+	return move
+}
---
+++ src/main.ts
@@ -0,0 +1,261 @@
+import './style.css'
+import { Chessground } from 'chessground'
+import { Chess, type Move, SQUARES } from 'chess.js'
+
+import type { Key, Piece } from 'chessground/types';
+import "/node_modules/chessground/assets/chessground.base.css"
+import "/node_modules/chessground/assets/chessground.brown.css"
+import "/node_modules/chessground/assets/chessground.cburnett.css"
+import playLlama from './llama';
+
+let player: "white" | "black" = "white"
+
+function showResult(chess: Chess) {
+  if (chess.isCheckmate()) {
+    setTimeout(() => {
+      alert((chess.turn() == player.charAt(0) ? "Llama" : " You") + " Won!")
+    }, 1500)
+  } else {
+    setTimeout(() => {
+      alert("It's a draw!")
+    }, 1500)
+  }
+}
+
+
+function getValidMap(chess: Chess): Map<Key, Key[]> {
+  const dests = new Map();
+  SQUARES.forEach(s => {
+    const ms = chess.moves({ square: s, verbose: true });
+    if (ms.length) dests.set(s, ms.map(m => m.to));
+  });
+  return dests;
+}
+
+const isPromotion = (dest: Key, piece: Piece): boolean => {
+  return (
+    piece.role == "pawn" &&
+    ((piece.color == "white" && dest[1] == "8") ||
+      (piece.color == "black" && dest[1] == "1"))
+  );
+};
+
+const chess = new Chess();
+
+const moves: string[] = [];
+
+const movesContainer = document.getElementById("moves")!;
+
+function movesRowBuilder(move: Move) {
+  const moveDiv = document.createElement("div");
+  moveDiv.classList.add("move-san");
+  moveDiv.innerText = move.san;
+  let lastRow = movesContainer.querySelector(".move:last-of-type")
+  if (lastRow !== null && lastRow.childElementCount === 2) {
+    lastRow.appendChild(moveDiv);
+  } else {
+    const row = document.createElement("div")
+    row.classList.add("move")
+
+    const index = document.createElement("div");
+    index.classList.add("move-index");
+    index.innerText = `${Math.floor(moves.length / 2)}`;
+
+    row.append(index, moveDiv);
+    lastRow = row;
+    movesContainer.appendChild(row);
+  }
+}
+
+const promotionDialog = document.getElementById("promotionDialog")! as HTMLDialogElement;
+
+function showPromotionDialog(): Promise<string> {
+  return new Promise((resolve) => {
+    promotionDialog.showModal();
+
+    const buttons = promotionDialog.querySelectorAll("button");
+    buttons.forEach(button => {
+      button.onclick = () => {
+        promotionDialog.close();
+        resolve(button.value!);
+      }
+    });
+  });
+}
+
+function check(): boolean | "white" | "black" {
+  return chess.isCheck() ? chess.turn() === "w" ? "white" : "black" : false;
+}
+
+async function llamaMove() {
+  const dests = getValidMap(chess);
+  const move = await playLlama(moves, dests);
+  console.log(move);
+  moves.push(move);
+  let oppMove = chess.move(move);
+
+  movesRowBuilder(oppMove);
+
+  cg.set({
+    fen: chess.fen(),
+    movable: {
+      color: player,
+      free: false,
+      dests: getValidMap(chess)
+    },
+    turnColor: player,
+    check: check()
+  });
+
+  if (chess.isGameOver()) {
+    showResult(chess);
+  }
+}
+
+const cg = Chessground(document.getElementById("app")!, {
+  coordinatesOnSquares: true,
+  movable: {
+    color: player,
+    free: false,
+    dests: getValidMap(chess),
+    events: {
+      after: async (orig, dest) => {
+        let piece = cg.state.pieces.get(dest);
+        let playerMove: Move;
+
+        if (isPromotion(dest, piece!)) {
+          let promotion = await showPromotionDialog();
+          playerMove = chess.move({ from: orig, to: dest, promotion: promotion })
+          moves.push(orig + dest + "q");
+        } else {
+          playerMove = chess.move({ from: orig, to: dest })
+          moves.push(orig + dest);
+        }
+
+        movesRowBuilder(playerMove);
+
+        cg.set({
+          fen: chess.fen(),
+          turnColor: player == "white" ? "black" : "white",
+          check: check()
+        });
+
+        if (chess.isGameOver()) {
+          showResult(chess);
+          return;
+        }
+
+        llamaMove();
+      }
+    }
+  },
+  fen: chess.fen(),
+  check: check(),
+  highlight: {
+    check: true
+  }
+})
+
+document.getElementById("undo")!.onclick = async () => {
+  let popCount = 0;
+  if (player === "white") {
+    popCount = ((moves.length - 1) % 2) === 1 ? 1 : 2;
+  } else {
+    popCount = ((moves.length - 1) % 2) === 0 ? 1 : 2;
+  }
+
+  while (popCount--) {
+    let lastMove = movesContainer.querySelector(".move:last-of-type");
+    if (lastMove === null) break;
+
+    let lastHalfMove = lastMove?.querySelector(".move-san:last-of-type");
+    if (lastHalfMove !== null) {
+      chess.undo();
+      moves.pop();
+      lastMove?.removeChild(lastHalfMove);
+    }
+    if (lastMove?.childElementCount === 1) {
+      movesContainer.removeChild(lastMove);
+    }
+  }
+
+  cg.set({
+    fen: chess.fen(),
+    viewOnly: false, movable: {
+      color: player,
+      free: false,
+      dests: getValidMap(chess)
+    },
+    check: check(),
+    turnColor: player,
+  });
+
+  if (moves.length === 1 && player === "black") {
+    llamaMove();
+  }
+}
+
+async function initGame(playerColor: "white" | "black", state: string[]) {
+  chess.reset();
+  moves.splice(0, moves.length);
+  let promotionOptions = promotionDialog.getElementsByClassName("piece")!;
+
+  player = playerColor;
+
+  if (player === "white") {
+    moves.push("0-1");
+    for (let option of promotionOptions) {
+      option.classList.remove("black");
+    }
+  } else {
+    moves.push("1-0");
+    for (let option of promotionOptions) {
+      option.classList.add("black");
+    }
+  }
+
+  for (let move of state) {
+    moves.push(move);
+    let m = chess.move(move);
+    movesRowBuilder(m);
+  }
+
+  cg.set({
+    orientation: player,
+    fen: chess.fen(),
+    turnColor: player,
+    movable: {
+      color: player,
+      free: false,
+      dests: getValidMap(chess)
+    },
+    check: check(),
+  })
+
+  if (player === "black" && (state.length % 2) === 0) {
+    llamaMove();
+  }
+}
+
+// initGame("black", ["e2e4", "g8f6", "b1c3", "d7d5", "e4e5", "d5d4", "e5f6", "d4c3", "d2d4", "c3b2", "f6g7",])
+
+const colorDialog = document.getElementById("colorDialog")! as HTMLDialogElement;
+const blackButton = document.getElementById("blackSelection")! as HTMLButtonElement;
+const whiteButton = document.getElementById("whiteSelection")! as HTMLButtonElement;
+
+blackButton.onclick = () => {
+  colorDialog.close();
+  colorDialog.returnValue = "black";
+  initGame("black", []);
+}
+whiteButton.onclick = () => {
+  colorDialog.close();
+  colorDialog.returnValue = "white";
+  initGame("white", []);
+}
+
+function startGame() {
+  colorDialog.showModal();
+}
+
+startGame();
